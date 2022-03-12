49991dc11e1dff0b42f404b3c8f4e4deff00e386
yaqoubalghadban <yoyob4654@gmail.com>
2022-03-12 12:42:47 +0300

    done

--- ios-cw-9/ios-cw-9/ContentView.swift
+++ ios-cw-9/ios-cw-9/ContentView.swift
@@ -39,7 +39,20 @@ struct ContentView: View {
                         // if statement
                         // here يجب أن تتغير الصورة على حسب الدرجة
                         // image : الصور موجودة في assets
+if (Double(enterGrade) ?? 0 >= 90){
+    grade="a"
+    
+}
+    else if (Double(enterGrade) ?? 0 >= 80){
+        grade="b"}
                         
+        else if (Double(enterGrade) ?? 0 >= 70){
+            grade="c"}
+                        else if (Double(enterGrade) ?? 0 >= 60){
+                            grade="d"}
+        else  {
+                            grade="f"}
+            
                     }
                 
                 Text("لقد حصلت على درجة")
